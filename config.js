a3bb246340dec16b2b00b4939ad61ce16f8b3ea7
mohanedmagdii <mohaned-magdy@hotmail.com>
2018-06-06 02:06:18 +0200

    api

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 module.exports = {
 
+    // mysql://b1466f0b4aad25:0bdc5aef@us-cdbr-iron-east-04.cleardb.net/heroku_22fdd806fc12bbe?reconnect=true
 
     liveDb : {
-        host: 'localhost',
-        user: 'root',
-        pass:'',
+        host: 'us-cdbr-iron-east-04.cleardb.net',
+        user: 'b1466f0b4aad25',
+        pass:'0bdc5aef',
         port:3306,
-        database:'vondos',
+        database:'heroku_22fdd806fc12bbe',
         dialect:'mysql',
         pool:{
             min:0,
